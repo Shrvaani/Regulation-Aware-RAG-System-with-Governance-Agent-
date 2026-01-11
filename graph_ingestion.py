e869e7c7b02d559b434f7a0018b2163a65ed4869
Shrvaani <sshrvaani@gmail.com>
2026-01-11 16:05:27 +0530

    Fix langchain text_splitter to LangChain versions

--- graph_ingestion.py
+++ graph_ingestion.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import TextLoader, PyPDFLoader, UnstructuredPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import networkx as nx
 import json
 import os
